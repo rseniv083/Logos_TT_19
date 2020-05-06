8b7961d45e1e0f620386d13e87f1a69fb549d369
Roma Seniv <rseniv083@gmail.com>
2020-05-06 19:52:33 +0300

    fixed some bugs

--- homeworks_js/homework_№8_Seniv/task2/main.js
+++ homeworks_js/homework_№8_Seniv/task2/main.js
@@ -1,17 +1,17 @@
-let r = prompt('Write a radius');
 
 function getArea(radius){
     let s = Math.PI * Math.pow(radius, 2);
     alert(Math.round(s) + ' square units');
 }
 
-if(r == null){
-    alert('Please, write a radius');
-    prompt('Write a radius');
+let r = prompt('Write a radius');
+while(r != +r){
+    if(r == null){
+        alert('Please, write a radius'); 
+    }
+    else{
+        alert('Just number, please')
+    }
+    r = prompt('Write a radius');
 }
-else if(r == +r){
-    getArea(r);
-}
-else{
-    alert('Please, write a number');
-}
+getArea(r);
