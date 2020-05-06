6a35f8fd59aa2a1650df4262b625db1e7e87e93c
Roma Seniv <rseniv083@gmail.com>
2020-05-06 20:08:27 +0300

    fixed some bugs

--- homeworks_js/homework_№8_Seniv/task3/main.js
+++ homeworks_js/homework_№8_Seniv/task3/main.js
@@ -1,19 +1,21 @@
+
+function getSqrt(number) {
+  alert(Math.sqrt(number));
+}
+
 let n = prompt('Write a number');
-
-function getSqrt(number){
-    alert(Math.sqrt(number));
+if (n <= 0) {
+    alert("Please, write a positive number");
+    n = prompt("Write a number");
+}
+while (n != +n) {
+  if (n == null) {
+    alert("Please, write a number");
+  }
+  else {
+    alert("Just number, please");
+  }
+  n = prompt("Write a number");
 }
 
-if(n == null){
-    alert('Please, write a number');
-    prompt('Write a number');
-}
-else if(n <= 0){
-    alert('Please, write a positive number');
-}
-else if(n == +n){
-    getSqrt(n);
-}
-else{
-    alert('Please, write a number');
-}
+getSqrt(n);
