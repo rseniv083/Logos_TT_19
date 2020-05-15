1b2e0a579ee06ecfe02a8e2fce49a06e6b3c7d02
Roma Seniv <rseniv083@gmail.com>
2020-05-15 23:21:55 +0300

    added: homework 13, task 1

--- homeworks_js/homework_№13_Seniv/task1/main.js
+++ homeworks_js/homework_№13_Seniv/task1/main.js
@@ -1,48 +1,65 @@
-const color = document.querySelector('.color');
-const image = document.querySelector('.image');
+const color = document.querySelector(".color");
+const image = document.querySelector(".image");
 
-function createContainerColor(){
-    let container = document.createElement('div');
-    container.classList.add('container');
-    for(let i = 0; i < 9; i++){
-        let backgroundColors = ['red', 'blue', 'violet', 'green', 'chocolate', 'gold', 'silver', 'pink', 'brown'];
-        let childBox = document.createElement('div');
-        childBox.style.backgroundColor = backgroundColors[i];
-        childBox.classList.add('childBox');
-        childBox.onclick = function(){
-            document.body.style.backgroundColor = backgroundColors[i];
-            if(image.onclick){
-                document.body.style.backgroundImage = 'none';
-            }
-        }
-        container.appendChild(childBox);
-    }
-    document.body.appendChild(container);
-    
+function createContainerColor() {
+  let container = document.createElement("div");
+  container.classList.add("container");
+  for (let i = 0; i < 9; i++) {
+    let backgroundColors = [
+      "red",
+      "blue",
+      "violet",
+      "green",
+      "chocolate",
+      "gold",
+      "silver",
+      "pink",
+      "brown",
+    ];
+    let childBox = document.createElement("div");
+    childBox.style.backgroundColor = backgroundColors[i];
+    childBox.classList.add("childBox");
+    childBox.onclick = function () {
+      document.body.style.backgroundColor = backgroundColors[i];
+      if (image.onclick) {
+        document.body.style.backgroundImage = "none";
+      }
+    };
+    container.appendChild(childBox);
+  }
+  document.body.appendChild(container);
 }
 
-function createContainerImage(){
-    let container = document.createElement('div');
-    container.classList.add('container');
-    for(let i = 0; i < 9; i++){
-        let backgroundImages = ['url(img/1.jpeg)', 'url(img/2.jpeg)', 'url(img/3.jpeg)', 'url(img/4.jpeg)', 'url(img/5.jpeg)', 'url(img/6.jpeg)', 'url(img/7.jpeg)', 'url(img/8.jpeg)','url(img/9.jpeg)']
-        let childBox = document.createElement('div');
-        childBox.style.backgroundImage = backgroundImages[i];
-        childBox.classList.add('childBox');
-        childBox.style.backgroundSize = 'cover';
-        childBox.onclick = function(){
-            document.body.style.backgroundImage = backgroundImages[i];
-            document.body.style.backgroundSize = 'cover';
-        }
-        container.appendChild(childBox);
-    }
-    document.body.appendChild(container);
+function createContainerImage() {
+  let container = document.createElement("div");
+  container.classList.add("container");
+  for (let i = 0; i < 9; i++) {
+    let backgroundImages = [
+      "url(img/1.jpeg)",
+      "url(img/2.jpeg)",
+      "url(img/3.jpeg)",
+      "url(img/4.jpeg)",
+      "url(img/5.jpeg)",
+      "url(img/6.jpeg)",
+      "url(img/7.jpeg)",
+      "url(img/8.jpeg)",
+      "url(img/9.jpeg)",
+    ];
+    let childBox = document.createElement("div");
+    childBox.style.backgroundImage = backgroundImages[i];
+    childBox.classList.add("childBox");
+    childBox.style.backgroundSize = "cover";
+    childBox.onclick = function () {
+      document.body.style.backgroundImage = backgroundImages[i];
+      document.body.style.backgroundSize = "cover";
+    };
+    container.appendChild(childBox);
+  }
+  document.body.appendChild(container);
 }
 
 color.onclick = createContainerColor;
 image.onclick = createContainerImage;
-
-
 
 
 
