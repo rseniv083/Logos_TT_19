80f42cfa16981534e2b1f914704371f91197f769
Roma Seniv <rseniv083@gmail.com>
2020-05-31 23:18:56 +0300

    code refactored

--- homeworks_js/homework_№19_Seniv/main.js
+++ homeworks_js/homework_№19_Seniv/main.js
@@ -1,34 +1,28 @@
-let accountForm = document.forms["accountForm"];
+const accountForm = document.forms["accountForm"];
 
-const name = /^[a-zA-Z]{1,20}$/;
-const email = /^[\w,.-]+@[\w,.-]+$/;
-const phone = /^\+38\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
 $(".phone_mask").mask("+38(999)999-99-99");
-const pass = /^\w{8,15}$/;
 
 accountForm.signUp.addEventListener("click", function () {
-  const firstName = accountForm.firstName;
-  const secondName = accountForm.secondName;
-  const emailAdress = accountForm.email;
-  const phoneNumber = accountForm.phoneNumber;
-  const password = accountForm.password;
+  const name = /^[a-zA-Z]{1,20}$/;
+  const email = /^[\w,.-]+@[\w,.-]+$/;
+  const phone = /^\+38\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
+  const pass = /^\w{8,15}$/;
 
-  coincidence(firstName, name);
-  coincidence(secondName, name);
-  coincidence(emailAdress, email);
-  coincidence(phoneNumber, phone);
-  coincidence(password, pass);
+  coincidence(accountForm.firstName, name);
+  coincidence(accountForm.secondName, name);
+  coincidence(accountForm.email, email);
+  coincidence(accountForm.phoneNumber, phone);
+  coincidence(accountForm.password, pass);
 });
 
 function coincidence(element, regExp) {
-  let className = element.name;
-  let label = document.getElementsByClassName(`${className}`);
+  const label = document.querySelector(`.${element.name}`);
 
   if (element.value.match(regExp) == null) {
     element.style.borderColor = "red";
-    label[0].style.display = "block";
+    label.style.display = "block";
   } else {
     element.style.borderColor = "green";
-    label[0].style.display = "none";
+    label.style.display = "none";
   }
 }
